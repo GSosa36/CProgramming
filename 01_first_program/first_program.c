7b7115481456ab1dc9eb9bc0689e8c56122d44e4
GSosa36 <gsosa8923@floridapoly.edu>
2025-01-16 16:26:45 +0000

    Implemented first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,23 @@
+// one-line comment 
+// This is the first program which prints a welcome message.
+
+/*
+
+
+Multiline comment.
+
+January 9th 2025.
+
+
+*/
+
+#include <stdio.h>
+
+
+// Create a main function
+int main() {
+    printf("Welcome\nto\nCOP2271!\n");
+    printf("I\nwant\nto\ncode!\n");
+
+    return 0;
+}
